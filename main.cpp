91e818c90bc983cdd26a3afe06ce456f5185295a
rkiii-code <ryu4649ryu.0114@icloud.com>
2025-06-04 02:13:35 +0900

    update all

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+#include "include/des.hpp"
 
 int main(){
     std::vector<uint8_t> key = {0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1};
--- src/des.cpp
+++ src/des.cpp
@@ -1,7 +1,7 @@
 #include "../include/des.hpp"
 
 namespace des{
-    const uint8_t ip_table[64] = {
+    const uint8_t IP[64] = {
         57, 49, 41, 33, 25, 17, 9, 1,
         59, 51, 43, 35, 27, 19, 11, 3,
         61, 53, 45, 37, 29, 21, 13, 5,
@@ -12,6 +12,17 @@ namespace des{
         62, 54, 46, 38, 30, 22, 14, 6
     };
     
+    const uint8_t IP_inverse[64] = {
+        39,  7, 47, 15, 55, 23, 63, 31,
+        38,  6, 46, 14, 54, 22, 62, 30,
+        37,  5, 45, 13, 53, 21, 61, 29,
+        36,  4, 44, 12, 52, 20, 60, 28,
+        35,  3, 43, 11, 51, 19, 59, 27,
+        34,  2, 42, 10, 50, 18, 58, 26,
+        33,  1, 41,  9, 49, 17, 57, 25,
+        32,  0, 40,  8, 48, 16, 56, 24
+    };
+
     const uint8_t PC1[56] = {
         56, 48, 40, 32, 24, 16,  8,
         0 , 57, 49, 41, 33, 25, 17,
@@ -32,14 +43,36 @@ namespace des{
         29, 39, 50, 44, 32, 47,
         43, 48, 38, 55, 33, 52,
         46, 41, 49, 35, 28, 31
-    }
+    };
+
+    const uint8_t E[48] = {
+        31,  0,  1,  2,  3,  4,
+         3,  4,  5,  6,  7,  8,
+         7,  8,  9, 10, 11, 12,
+        11, 12, 13, 14, 15, 16,
+        15, 16, 17, 18, 19, 20,
+        19, 20, 21, 22, 23, 24,
+        23, 24, 25, 26, 27, 28,
+        27, 28, 29, 30, 31, 0
+    };
+
+    const uint8_t P[32] = {
+        15,  6, 19, 20, 
+        28, 11, 27, 16,
+         0, 14, 22, 25, 
+         4, 17, 30,  9,
+         1,  7, 23, 13,
+        31, 26,  2,  8,  
+        18, 12, 29,  5,
+        21, 10,  3, 24
+    };
 
     uint64_t left_shift(uint64_t key, int shift) {
-        // 左シフトを行う
+        // 左シフトを行うsssssssssssssssssssssssssssss
         return ((key << shift) | (key >> (28 - shift))) & 0x0FFFFFFF;
     }
-    std::vector<uint_64> generate_subkeys(uint64_t subkey){
-        std::vector<uint_64> subkeys(16);
+    std::vector<uint64_t> generate_subkeys(uint64_t subkey){
+        std::vector<uint64_t> subkeys(16);
         uint64_t key56 = permute_pc1(subkey);
         uint64_t c = (key56 >> 28) & 0x0FFFFFFF; // 左半分
         uint64_t d = key56 & 0x0FFFFFFF; // 右半分
@@ -63,7 +96,7 @@ namespace des{
         // 各ビット位置について転置を適用
         for (int i = 0; i < 64; i++) {
             // IPテーブルの値は1-64の範囲だが、ビット操作は0-63の範囲で行うため調整
-            uint8_t source_pos = ip_table[i];
+            uint8_t source_pos = IP[i];
             
             // sourceの位置のビットを取り出す
             uint64_t bit = (block >> source_pos) & 0x01;
